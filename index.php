74a20a1e01929bd63b1bc054c4b58a41f7b86325
Chandramouli333 <71500064+Chandramouli333@users.noreply.github.com>
2021-07-17 12:59:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+	body {
+  background-image: url('https://d194ip2226q57d.cloudfront.net/images/Guaranty-Bank_Farmers-Park_First-.729a3d66.fill-1200x650.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+}
+#a{
+	font-size:80px;
+	text-align:center;
+	color:FFFF00;
+	font-style:bold;
+}
+
+.my-button {
+    display: inline-block;
+    margin: 15px; /* space between buttons */
+    background: #FFCDD2; /* background color */
+    color: #CC0000; /* text color */
+    font-size: 2.0em;
+    font-family: ‘Georgia’, serif;
+    font-style: bold;
+    border-radius: 70px; /* rounded corners */
+    padding: 8px 16px; /* space around text */
+    -moz-transition: all 0.2s;
+    -webkit-transition: all 0.2s;
+    transition: all 0.2s;
+}
+.my-button:hover {
+    background: #666;
+    color: #FFF8E1;
+}
+#d{
+	font-size:22px;
+	text-align:center;
+	color:33FF33;
+	font-style:bold;
+}
+
+</style>
+</head>
+
+<body>
+<p id="a">MOULI&nbsp&nbsp BANKING &nbsp&nbsp SYSTEM</p><br><br><br><br><br><br>
+<center><a class="my-button" title="Relevant Title" href="users.php">our customers</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a class="my-button" title="Relevant Title" href="users.php">transfer money</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a class="my-button" title="Relevant Title" href="transactions.php">transaction history</a></center>
+</body>
+</html><br><br><br><br><br><br><br><br><br>
+    <footer id="d">
+            <p >&COPY 2021 <b>CHANDRA MOULI</b> <br>CEO,S-BANK</p>
+        </footer>
+             
+</body>
+</html>
